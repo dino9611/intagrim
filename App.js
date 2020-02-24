1749a3ab9f69211f0a851be4c2a092a943714b51
Dino <aldinorahman36@gmail.com>
2020-02-24 09:28:39 +0700

    new

---
+++ App.js
@@ -0,0 +1,18 @@
+import React,{Component} from 'react';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import reduxThunk from 'redux-thunk';
+import reducers from './src/redux/reducers'
+import AppInit from './AppInit'
+
+const App = () => {
+  return (
+    <Provider store={createStore(reducers, {}, applyMiddleware(reduxThunk))}>
+      <AppInit />
+    </Provider>
+  );
+};
+
+
+
+export default App;
