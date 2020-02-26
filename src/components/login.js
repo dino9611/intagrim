4212b850af335c408b38a65bf32cbd780a1caec6
Dino <aldinorahman36@gmail.com>
2020-02-26 10:12:21 +0700

    dasd

--- src/components/login.js
+++ src/components/login.js
@@ -3,9 +3,21 @@ import {View,StatusBar} from 'react-native'
 import { Text, Input, Icon, Button } from 'react-native-elements';
 import style from './../style/style'
 import * as Animatable from 'react-native-animatable'
+
 import {CommonActions} from '@react-navigation/native'
 
+const reducers=(state,action)=>{
+    switch(action.type){
+        case 'Change-data':
+            return {...state,[action.name]:action.payload};
+        default:
+            return state
+    }
+}
+
+
 const Login=(props)=>{
+    const [state,dispatch]=useReducer(reducers,{email:'',password:'',passHidden:true})
     // useEffect(()=>{
     //     const resetcomm=CommonActions.reset({
     //         index:0,
@@ -15,15 +27,70 @@ const Login=(props)=>{
     //     })
     //     props.navigation.dispatch(resetcomm)
     // },[])
-    const [passHidden,setpassHidden]=useState(true)
+    // const [passHidden,setpassHidden]=useState(true)
     // const [conpassHidden,setconpassHidden]=useState(true)
 
     // const Gotohome=()=>{
     //     props.navigation.navigate('Drawermain')
     // }
+    console.log(state)
     return(
         <View style={style.LogcontainerStyle}>
-            
+            <StatusBar backgroundColor='teal'/>
+            <Animatable.Text animation={'fadeInDown'} duration={2000}>
+                <Text h3>Instagrim</Text>
+            </Animatable.Text>
+            <View style={style.LoginputStyle}>
+                <Input
+                    placeholder='Email'
+                    leftIcon={
+                        <Icon
+                            name='email'
+                            size={24}
+                            color='black'
+                        />
+                    }
+                    value={state.email}
+                    onChangeText={(text)=>dispatch({type:'Change-data',name:'email',payload:text})}
+                />
+                <Input
+                    placeholder='Password'
+                    leftIcon={
+                        <Icon
+                            name='lock'
+                            size={24}
+                            color='black'
+                        />
+                    }
+                    rightIcon={
+                        <Icon
+                            name={state.passHidden?'visibility-off':'visibility'}
+                            size={24}
+                            color={state.passHidden ? '#bfc3c9' : 'black'}
+                            onPress={()=>dispatch({type:'Change-data',name:'passHidden',payload:!state.passHidden})}
+                        />
+                    }
+                    secureTextEntry={state.passHidden}
+                    value={state.password}
+                    onChangeText={(text)=>dispatch({type:'Change-data',name:'password',payload:text})}
+                />
+            </View>
+
+            <Button
+                title="Login"
+                containerStyle={{ width: '95%', marginBottom: 10 }}
+                buttonStyle={{ backgroundColor: 'black' }}
+                // loading={this.props.loading}
+                // onPress={this.onBtnLoginPress}
+            />
+            <Button
+                title="Go to Register"
+                containerStyle={{ width: '95%' }}
+                buttonStyle={{ borderColor: 'black', borderWidth: 1 }}
+                titleStyle={{ color: 'black' }}
+                type="outline"
+                onPress={() => props.navigation.navigate('Register')}
+            />
         </View>
     )
 }
--- src/components/register.js
+++ src/components/register.js
@@ -1,12 +1,25 @@
-import React,{useState} from 'react';
+import React,{useState, useReducer} from 'react';
 import {View} from 'react-native'
 import { Text, Input, Icon, Button } from 'react-native-elements';
 import style from './../style/style'
 import * as Animatable from 'react-native-animatable';
+import {connect} from 'react-redux'
+import {onUserRegister} from './../redux/actions'
+
+
+const reducers=(state,action)=>{
+    switch(action.type){
+        case 'Change-data':
+            return {...state,[action.name]:action.payload};
+        default:
+            return state
+    }
+}
+
 
 // fbec40e5-2d70-4792-841e-f54876f1cdaa
 const Register=({navigation})=>{
-
+    const [state,dispatch]=useReducer(reducers,{email:'',username:'',password:'',conpassword:''})
     const [passHidden,setpassHidden]=useState(true)
     const [conpassHidden,setconpassHidden]=useState(true)
     return(
@@ -24,6 +37,8 @@ const Register=({navigation})=>{
                             color='black'
                         />
                     }
+                    value={state.email}
+                    onChangeText={(text)=>dispatch({type:'Change-data',name:'email',payload:text})}
                 />
                 <Input
                     placeholder='Username'
@@ -34,6 +49,8 @@ const Register=({navigation})=>{
                             color='black'
                         />
                     }
+                    value={state.username}
+                    onChangeText={(text)=>dispatch({type:'Change-data',name:'username',payload:text})}
                 />
                 <Input
                     placeholder='Password'
@@ -53,6 +70,8 @@ const Register=({navigation})=>{
                         />
                     }
                     secureTextEntry={passHidden}
+                    value={state.password}
+                    onChangeText={(text)=>dispatch({type:'Change-data',name:'password',payload:text})}
                 />
                 <Input
                     placeholder='Confirm Password'
@@ -72,8 +91,17 @@ const Register=({navigation})=>{
                         />
                     }
                     secureTextEntry={conpassHidden}
+                    value={state.conpassword}
+                    onChangeText={(text)=>dispatch({type:'Change-data',name:'conpassword',payload:text})}
                 />
             </View>
+            <Button
+                    title="Register"
+                    containerStyle={{ width: '95%', marginBottom: 10 }}
+                    buttonStyle={{ backgroundColor: 'black' }}
+                    // onPress={this.onBtnRegisterPress}
+                    // loading={true}
+                />
             <Button
                 title="Back to Login"
                 containerStyle={{ width: '95%' }}
@@ -85,48 +113,6 @@ const Register=({navigation})=>{
         </View>
     )
 }   
-// const Register=({navigation})=>{
-              
-//     const [Email,setEmail]=useState('')
-//     return(
-//         <View style={style.RegcontainerStyle}>
-//             <Animatable.Text animation={'fadeInDown'} duration={2000}>
-//                 <Text h3>Register</Text>
-//             </Animatable.Text>
-//             <View style={style.ReginputStyle}>
-//                 <Input
-//                     placeholder='Email'
-//                     leftIcon={
-//                         <Icon
-//                             name='email'
-//                             size={24}
-//                             color='black'
-//                         />
-//                     }
-//                     value={Email}
-//                     onChangeText={(text)=>setEmail(text)}
-//                 />
-//                 <Input
-//                     placeholder='Username'
-//                     leftIcon={
-//                         <Icon
-//                             name='account-box'
-//                             size={24}
-//                             color='black'
-//                         />
-//                     }
-//                 />
-//                  <Button
-//                     title="Back to Login"
-//                     containerStyle={{ width: '95%' }}
-//                     buttonStyle={{ borderColor: 'black', borderWidth: 1 }}
-//                     titleStyle={{ color: 'black' }}
-//                     type="outline"
-//                     onPress={() => navigation.goBack()}
-//                 />
-//             </View>
-//         </View>
-//     )
-// }
+
 
 export default Register
